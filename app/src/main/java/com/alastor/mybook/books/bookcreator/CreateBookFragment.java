9f2d0e317613c039405cf39e940ed5e48bc20102
Maciek <maciej.smolik95@gmail.com>
2020-10-04 17:36:36 +0200

    Creation of book

--- app/src/main/java/com/alastor/mybook/books/bookcreator/CreateBookFragment.java
+++ app/src/main/java/com/alastor/mybook/books/bookcreator/CreateBookFragment.java
@@ -1,6 +1,7 @@
 package com.alastor.mybook.books.bookcreator;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.alastor.mybook.FragmentAdministrator;
 import com.alastor.mybook.databinding.CreateBookFragmentBinding;
+import com.alastor.mybook.repository.api.model.Book;
 
 public class CreateBookFragment extends Fragment {
 
@@ -32,11 +35,42 @@ public class CreateBookFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         createBookViewModel = new ViewModelProvider(this).get(CreateBookViewModel.class);
+        createBookFragmentBinding.save.setOnClickListener(v -> {
+            final Book book = createNewBook();
+            makeRequest(book);
+        });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         createBookFragmentBinding = null;
+    }
+
+    private Book createNewBook() {
+        final String title = createBookFragmentBinding.title.getText().toString();
+        final String description = createBookFragmentBinding.description.getText().toString();
+        final String coverUrlText = createBookFragmentBinding.coverUrl.getText().toString();
+        return new Book(title, description, coverUrlText);
+    }
+
+    private void makeRequest(Book book) {
+        createBookViewModel.createBoot(book).observe(getViewLifecycleOwner(), stringResponse -> {
+            if (stringResponse == null) {
+                return;
+            }
+            switch (stringResponse.status) {
+                case LOADING:
+                    createBookFragmentBinding.createBookFlipper.setDisplayedChild(1);
+                    break;
+                case SUCCESS:
+                    Log.e("TAG", "makeRequest: " + stringResponse.data);
+                    createBookFragmentBinding.createBookFlipper.setDisplayedChild(0);
+                    FragmentAdministrator.popBackStack(getParentFragmentManager());
+                    break;
+                case ERROR:
+                    break;
+            }
+        });
     }
 }
--- app/src/main/java/com/alastor/mybook/books/bookcreator/CreateBookViewModel.java
+++ app/src/main/java/com/alastor/mybook/books/bookcreator/CreateBookViewModel.java
@@ -1,7 +1,63 @@
 package com.alastor.mybook.books.bookcreator;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
+import android.util.Log;
 
-public class CreateBookViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.alastor.mybook.Response;
+import com.alastor.mybook.login.LoginRepository;
+import com.alastor.mybook.repository.BookRepository;
+import com.alastor.mybook.repository.api.model.Book;
+
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class CreateBookViewModel extends AndroidViewModel {
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+    private BookRepository bookRepository = BookRepository.getInstance();
+    private LoginRepository loginRepository = LoginRepository.getInstance();
+
+    private MutableLiveData<Response<String>> createBookLiveData = new MutableLiveData<>();
+
+    public CreateBookViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<Response<String>> createBoot(Book book) {
+        bookRepository
+                .createBook(loginRepository.getNotExpiredToken(getApplication().getApplicationContext()), book)
+                .subscribeOn(Schedulers.io())
+                .subscribe(new SingleObserver<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable.add(d);
+                        createBookLiveData.postValue(Response.loading());
+                    }
+
+                    @Override
+                    public void onSuccess(String s) {
+                        createBookLiveData.postValue(Response.success(s));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("TAG", "onError: " + e);
+                    }
+                });
+
+        return createBookLiveData;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposable.clear();
+    }
 }
